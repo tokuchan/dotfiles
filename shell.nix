5ecae02d67d4231428329bf810102b2f22394ce1
Sean R. Spillane <sean@spillane.us>
2023-07-20 21:31:16 +0000

    Define NIX UNIDEV environment

---
+++ shell.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d0f2758381caca8b4fb4a6cac61721cc9de06bd9.tar.gz") {} }:
+
+pkgs.mkShell {
+  packages = [
+    pkgs.autojump
+    pkgs.asciidoctor-with-extensions
+    pkgs.bat
+    pkgs.emacs29
+    pkgs.exa
+    pkgs.figlet
+    pkgs.fish
+    pkgs.fzf
+    pkgs.gitFull
+    pkgs.gnumake
+    pkgs.jq
+    pkgs.keychain
+    pkgs.meld
+    pkgs.neovim
+    pkgs.qpdf
+    pkgs.ripgrep
+    pkgs.sqlite
+    pkgs.sshfs
+    pkgs.stow
+    pkgs.xclip
+  ];
+
+  shellHook = ''
+    export SHELL_TYPE="$SHELL_TYPE Unidev "
+    exec fish
+  '';
+
+  EDITOR = "nvim";
+  LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive";
+}
