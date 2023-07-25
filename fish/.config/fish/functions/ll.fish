df110867b624f91fa5d2493f360cacf09dda46e0
Sean R. Spillane <sean@spillane.us>
2023-07-25 15:40:04 +0000

    Try to fix ll

--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps='la -alhT --level=2' --wraps='la -lhT --level=2' --wraps='la -alhT' --wraps='la -alh' --description 'alias ll=la -alh'
-  la -alh $argv; 
+function ll --wraps='ls -lba' --description 'ls -lba'
+  la -lba $argv; 
 end
