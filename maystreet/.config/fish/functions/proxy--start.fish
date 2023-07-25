26a88b8ef98657f064f0e6b935b854b5edbf63de
Sean R. Spillane <sean@spillane.us>
2023-07-25 16:15:08 +0000

    Fix proxy--start function

--- maystreet/.config/fish/functions/proxy--start.fish
+++ maystreet/.config/fish/functions/proxy--start.fish
@@ -1,8 +1,6 @@
 function proxy--start --description 'Start Maystreet Proxy'
-pushd ./local/share/proxy
-pkill -f http.server
-nohup python3 -m http.server 80&
-sleep 1
-disown %1
+pushd ~/.local/share/proxy
+sudo pkill -f http.server
+sudo bash -c 'nohup python3 -m http.server 80& disown'
 popd
 end
