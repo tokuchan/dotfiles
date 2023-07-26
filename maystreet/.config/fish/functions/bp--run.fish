33666382ebb8932d08287c58402efa4ca7a1dcb4
Sean R. Spillane <sean@spillane.us>
2023-07-26 15:13:54 +0000

    Design robust bp--run script
    In this commit, I design a robust bp--run script. This script features a
    universal options parser of my own design, that should hopefully be fairly
    easy to use.

--- maystreet/.config/fish/functions/bp--run.fish
+++ maystreet/.config/fish/functions/bp--run.fish
@@ -1,12 +1,22 @@
 function bp--run --description 'Run command inside the specified container'
-argparse -is 'h/help' 'c/container=' -- $argv
-or return
-if test $_flag_help
-echo "Run a command inside the specified container.
--c / --container  Specify the container used.
--h / --help  Display this help.
-"
-else
-podman run -ti --entrypoint bash -v /home/seans/dev/bellport-stow:/home/seans/dev/bellport-stow -v (pwd):(pwd) $_flag_container -c "cd "(pwd)" && $argv"
-end
+	command bp--run $argv
+	#	argparse -is 'h/help' 'r/run-in-project-root' 'c/container=' -- $argv
+	#	or return
+	#	if test $_flag_help
+	#		echo "Run a command inside the specified container.
+	#		-c / --container            Specify the container used.
+	#		-r / --run-in-project-root  Run the command in the same directory in which .git is found.
+	#		-h / --help                 Display this help.
+	#		"
+	#	else
+	#		set -l theMounts \
+	#			-v $(readlink -f $HOME):$(readlink -f $HOME) \
+	#			-v (pwd):(pwd)
+	#
+	#		if test $_flag_run_in_project_root
+	#			podman run -ti --entrypoint bash $theMounts $_flag_container -c "cd "(git rev-parse --show-toplevel)" && $argv"
+	#		else
+	#			podman run -ti --entrypoint bash $theMounts $_flag_container -c "cd "(pwd)" && $argv"
+	#		end
+	#	end
 end
