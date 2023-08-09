a52a114e74975f453cc842e964fde516fcee25de
Sean R. Spillane <sean@spillane.us>
2023-08-09 18:14:07 +0000

    Define a little function to fix sound

--- fish/.config/fish/functions/pafix.fish
+++ fish/.config/fish/functions/pafix.fish
@@ -1,4 +1,4 @@
 function pafix --wraps='pactl set-default-sink (pactl list short sinks | cut -f2 | fzf)' --description 'alias pafix=pactl set-default-sink (pactl list short sinks | cut -f2 | fzf)'
-  pactl set-default-source (pactl list short sources | cut -f2 | fzf --border-label='Select source')
-  pactl set-default-sink (pactl list short sinks | cut -f2 | fzf --border-label='Select sink') #$argv
+  pactl set-default-source (pactl list short sources | cut -f2 | fzf --query='yeti' --border --prompt='Select source --> ')
+  pactl set-default-sink (pactl list short sinks | cut -f2 | fzf --query='kt' --border --prompt='Select sink --> ') #$argv
 end
