437e166368bc261082979e7f16cc1ca47346096a
Sean R. Spillane <sean@spillane.us>
2023-08-23 10:00:39 -0400

    [fish]: Comment out the homebrew stuff for now

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,12 +3,16 @@ if status is-interactive
 end
 
 # Set up variables for homebrew
-set -gx HOMEBREW_PREFIX "/opt/homebrew";
-set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
-set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
-set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+#set -gx HOMEBREW_PREFIX "/opt/homebrew";
+#set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+#set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+#set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+#set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+#set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+
+# Set up variables for homebrew
+#eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
 
 # Set up autojump
 if which autojump > /dev/null
