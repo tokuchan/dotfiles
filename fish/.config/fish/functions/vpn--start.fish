10ddc26f8ab8bdbdc3d8cc7a0d880eca7516808a
Sean R. Spillane <sean@spillane.us>
2023-10-04 10:06:25 -0400

    Try to fix up VPN function a bit

--- fish/.config/fish/functions/vpn--start.fish
+++ fish/.config/fish/functions/vpn--start.fish
@@ -1,19 +1,23 @@
 function vpn--start
-  echo "Is OpenVPN currently running?"
+  echo -e "\rIs OpenVPN currently running?"
   if pgrep -fa openvpn
-    echo "Yes, so I will kill it now."
+    echo \e "\rYes, so I will kill it now."
     sudo pkill -9 -f openvpn
   else
-    echo "No."
+    echo \e "\rNo."
   end
 
-  echo "Starting OpenVPN in daemon mode"
+  echo -e "\rStarting OpenVPN in daemon mode"
   sudo openvpn --config ~/.local/share/openvpn/maystreet.ovpn --daemon
   pgrep -fa openvpn
 
-  echo "Setting up DNS resolution"
-  while not sudo resolvectl dns tun0 10.98.16.41 10.98.16.42 2> /dev/null
+  echo -e "\rSetting up DNS resolution"
+  for i in seq 10 -1 0
+    if sudo resolvectl dns tun0 10.98.16.41 10.98.16.42 2> /dev/null
+        break
+    end
     echo -n .
     sleep 2
   end
+  echo -e '\rDone'
 end
