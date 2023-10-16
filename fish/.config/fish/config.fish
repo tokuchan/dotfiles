c9d17b3ef64601f751f13abe82b8f4ce878336e2
Sean R. Spillane <sean@spillane.us>
2023-10-16 15:06:34 -0400

    Clean up fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,29 +1,9 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# Ensure that dotfiles packages are available to run
+set -gx PATH $PATH ~/.local/bin
+set -gx MANPATH $MANPATH ~/.local/man
 
-# Clear PATH and set defaults
-#set -e PATH
-#set -gx PATH /home/seans/.local/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:/home/seans/.pyenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin
-
-# Set up variables for homebrew
-#set -gx HOMEBREW_PREFIX "/opt/homebrew";
-#set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-#set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-#set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
-#set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
-#set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
-
-# Set up variables for homebrew
-#eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-
-
-# Set up autojump
-if which autojump > /dev/null
-  source (which autojump | sed 's,bin.*$,,')/share/autojump/autojump.fish
-else
-  echo "Autojump not installed"
-end
+# Tell subprograms to use bash
+set -x SHELL /bin/bash
 
 # Set up pyenv
 set -gx PYENV_ROOT "$HOME/.pyenv"
@@ -32,12 +12,10 @@ if not command -v pyenv >/dev/null
 end
 pyenv init --no-rehash - | source
 
-# Tell subprograms to use bash
-set -x SHELL /bin/bash
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 
-# Created by `pipx` on 2022-03-14 15:48:49
-set PATH $PATH /Users/sspillane/.local/bin
-set PATH $PATH ~/.local/bin
+    # Set the terminal to xterm256-color by default
+    set -gx "TERM" "xterm-256color"
+end
 
-# Set the terminal to xterm256-color by default
-set -gx "TERM" "xterm-256color"
---
+++ unidev/.config/fish/conf.d/autojump.fish
@@ -0,0 +1,1 @@
+test -s ~/.local/share/autojump/autojump.fish; and source ~/.local/share/autojump/autojump.fish
