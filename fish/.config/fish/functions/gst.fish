642768a49fcd4dc5f42085339278fc6bcb0bc990
Sean R. Spillane <sean@spillane.us>
2024-03-04 16:30:10 -0500

    Add nice formatting to git status

--- fish/.config/fish/functions/gst.fish
+++ fish/.config/fish/functions/gst.fish
@@ -1,17 +1,22 @@
 function gst --wraps='g st' --description 'alias gst=g st'
   echo
-  echo Status:
+  echo Status
+  echo ======
   echo
   echo Smartlog:
   g sl 'stack()'
   echo
   echo Changes:
   echo
-  g changes;
+  g changes --color=always | head -15;
   echo
-  echo Patches:
-  echo
-  g patches;
-  echo
+  if git branch | grep patches
+  then
+    echo Patches:
+    echo
+    g patches --color=always | head -15;
+    echo -e'\033]0m'
+  end
+  echo Status
   g st $argv; 
 end
