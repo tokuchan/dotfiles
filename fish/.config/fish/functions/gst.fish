55ed121a724bf43395adf4e4b9ee852b3aafbd4d
Sean R. Spillane <sean@spillane.us>
2024-03-05 19:16:21 -0500

    Make gst look nicer

--- fish/.config/fish/functions/gst.fish
+++ fish/.config/fish/functions/gst.fish
@@ -1,22 +1,36 @@
 function gst --wraps='g st' --description 'alias gst=g st'
-  echo
-  echo Status
-  echo ======
-  echo
-  echo Smartlog:
-  g sl 'stack()'
-  echo
-  echo Changes:
-  echo
-  g changes --color=always | head -15;
-  echo
+  set -lx file /tmp/git-log-$fish_pid
+
+  function display
+    argparse -i 'title=' -- $argv
+    or return
+    printf "%s\n--------\n```\n" "$_flag_title" >> $file
+    for l in (printf "$argv")
+      printf "%s\n" $l >> $file
+    end
+    printf "```\n" >> $file
+  end
+
+  function hr
+    printf "\n---\n" >> $file
+  end
+
+  printf "Status\n========\n" > $file
+
+  display --title="SmartLog" (for l in (g sl 'stack()'); echo "$l\\n"; end)
+  hr
+  display --title="Changes" (for l in (g changes -n 15); echo "$l\\n"; end)
+
   if git branch | grep patches
   then
-    echo Patches:
-    echo
-    g patches --color=always | head -15;
-    echo -e'\033]0m'
+    hr
+    display --title="Patches" (for l in (g patches -n 15); echo "$l\\n"; end)
   end
-  echo Status
-  g st $argv; 
+
+  hr
+  display --title="Status" (for l in (g st); echo "$l\\n"; end)
+
+  cat $file | rich -a rounded -m -
+
+  rm -f $file
 end
