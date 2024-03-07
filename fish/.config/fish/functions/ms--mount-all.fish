9a39e57b483560fb4bc204dc1a037e51eb5b1643
Sean R. Spillane <sean@spillane.us>
2024-03-07 11:42:41 -0500

    Update ms--mount-all to use fzf
    In this commit, I update ms--mount-all to use fzf so I can select which things I actually want to mount.
    Eventually, I have to rename this thing.

--- fish/.config/fish/functions/ms--mount-all.fish
+++ fish/.config/fish/functions/ms--mount-all.fish
@@ -1,7 +1,11 @@
 function ms--mount-all --description 'Try to mount everything in ~/mnt/'
-for path in ~/mnt/*
-test -d $path || continue
-echo $path
-ms--mount $path
-end
+  for path in (find ~/mnt/ -maxdepth 1 -print | tail +2 | fzf -m)
+    if test -d "~/mnt/$path" 
+      printf "Skipping: %s\n" $path
+      continue
+    else
+      printf "Trying to mount: %s\n" $path
+      ms--mount $path
+    end
+  end
 end
