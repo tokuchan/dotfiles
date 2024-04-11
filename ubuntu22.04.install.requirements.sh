65ab913796446cc214a2cc2f0047bbc2eff33f9e
Sean R. Spillane <sean@spillane.us>
2024-04-11 16:50:16 -0400

    Convert package installer script to hold list

--- ubuntu22.04.install.requirements.sh
+++ ubuntu22.04.install.requirements.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 # Install requirements for Ubuntu 22.04 LTS
-cat ./ubuntu22.04.requirements.txt | xargs sudo apt install -yqq
+# Install base packages:
+sudo apt install -yqq \
+automake \
+build-essential \
+cmake \
+gcc-12 \
+gettext \
+git \
+make \
+ninja-build \
+pkg-config
+
+# Install libraries:
+sudo apt install -yqq \
+libcurl4-openssl-dev \
+libfontconfig-dev \
+libfreetype-dev \
+libglib2.0-dev \
+libgnutls28-dev \
+libgtk-3-dev \
+libncurses-dev \
+libssl-dev \
+libtool \
+libvterm-dev \
+libxml2-dev
+
+# Install utilities:
+sudo apt install -yqq \
+bat \
+curl \
+exa \
+fish \
+fzf \
+git-flow \
+kitty \
+podman \
+stow \
+unzip 
