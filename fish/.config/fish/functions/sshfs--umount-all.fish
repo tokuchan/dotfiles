ab889ff422891a439f6328974dc8ef88efe7bd0b
Sean R. Spillane <tokuchan@gmail.com>
2024-05-29 22:40:27 -0400

    Clean up sshfs--umount functions

--- fish/.config/fish/functions/sshfs--umount-all.fish
+++ fish/.config/fish/functions/sshfs--umount-all.fish
@@ -1,7 +1,11 @@
 function sshfs--umount-all --description 'Try to unmount everything in ~/mnt/'
-for path in ~/mnt/*
-test -d $path || continue
-echo $path
-sshfs--umount $path
-end
+  for path in (find ~/mnt/ -maxdepth 1 -print | tail +2 | fzf -m)
+    if test -d "~/mnt/$path"
+      printf "Skipping: %s\n" $path
+      continue
+    else
+      printf "Trying to unmount: %s\n" $path
+      sshfs--umount $path
+    end
+  end
 end
--- fish/.config/fish/functions/sshfs--umount.fish
+++ fish/.config/fish/functions/sshfs--umount.fish
@@ -1,3 +1,7 @@
-function sshfs--umount --description 'unmount the SSHFS filesystem'
+function sshfs--umount --description 'Unmount the given sshfs path'
+if test (uname) = "Linux"
 fusermount3 -u $argv
+else
+umount $argv
+end
 end
