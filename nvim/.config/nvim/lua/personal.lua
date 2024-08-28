1843d57b746908ef01834efaaeef21a0fb716730
Sean R. Spillane <tokuchan@gmail.com>
2024-08-28 12:34:45 -0400

    feat(nvim): Move personal configuration to lua
    In this commit, I move my personal nvim configuration to the lua
    subdirectory. Then, I require it from the astro-nvim stow. This way, I
    can start to separate out the features I want turned on from those I
    don't need.

--- nvim/.config/nvim/lua/personal.lua
+++ nvim/.config/nvim/lua/personal.lua
@@ -46,7 +46,7 @@ Plug 'easymotion/vim-easymotion'
 vim.call('plug#end')
 
 -- Fugitive: Configure gitlab plugin
-vim.g.fugitive_gitlab_domains = {'https://gitlab.svc.maystreet.com/'}
+--vim.g.fugitive_gitlab_domains = {'https://gitlab.svc.maystreet.com/'}
 
 -- Swap ; and :
 vim.cmd [[
@@ -154,101 +154,101 @@ vim.cmd [[
 ]]
 
 -- Set up leader menus.
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
-
-local status, wk = pcall(require, 'which-key')
-if not (status) then
-    vim.notify('Could not load which-key, skipping mappings.')
-    return
-end
-
-local wkm = {}
-
-wkm.b = {
-	name = 'buffers',
-	b = {'<Cmd>Telescope buffers<CR>', 'select a buffer from the list'},
-	v = {'<Cmd>rightbelow vsplit<CR><Cmd>Telescope buffers<CR>', 'select a buffer from the list, and open in a new vertical split'},
-	h = {'<Cmd>rightbelow split<CR><Cmd>Telescope buffers<CR>', 'select a buffer from the list, and open in a new horizontal split'},
-	d = {'<Cmd>bd<CR>', 'close the current buffer'},
-	n = {'<Cmd>bn<CR>', 'jump to the next buffer'},
-	p = {'<Cmd>bp<CR>', 'jump to the previous buffer'},
-}
-
-wkm.c = {
-	name = 'code and console',
-	c = {'<Cmd>botright split term://fish<CR>i', 'open a terminal console'},
-	f = {'<Cmd>call CocAction("format")<CR>', 'format the buffer'},
-	r = {'<Plug>(coc-rename)', 'rename the object under the cursor'},
-	i = {'<Cmd>CocCommand pyright.organizeimports<CR>', 'organize module imports'},
-}
-
-wkm.f = {
-	name = 'files',
-	f = {'<Cmd>Telescope find_files<CR>', 'find files'},
-	v = {'<Cmd>rightbelow vsplit<CR><Cmd>Telescope find_files<CR>', 'find file and open in new vertical split'},
-	V = {'<Cmd>vsplit<CR><Cmd>Telescope find_files<CR>', 'find file and open in new vertical split'},
-	h = {'<Cmd>rightbelow split<CR><Cmd>Telescope find_files<CR>', 'find file and open in new horizontal split'},
-	H = {'<Cmd>split<CR><Cmd>Telescope find_files<CR>', 'find file and open in new horizontal split'},
-	s = {'<Cmd>w<CR>', 'save to disk'},
-	c = {'<Cmd>rightbelow split $MYVIMRC<CR>', 'load vim initialization file'},
-	r = {'<Cmd>source $MYVIMRC<CR>', 'compile vim initialization'},
-	n = {
-		name = 'new-files',
-		s = {'<Cmd>:rightbelow new<CR>', 'split new buffer into window horizontally'},
-		v = {'<Cmd>:rightbelow vnew<CR>', 'split new buffer into window vertically'},
-	}
-}
-
-wkm.g = {
-	name = 'git',
-	g = {'<Cmd>:G<CR>', 'Open fugitive general buffer'}
-}
-
-wkm.j = {
-	name = 'jump',
-	j = {'<Plug>(easymotion-overwin-f2)', 'Jump the cursor to any point by typing two characters.'},
-}
-
-wkm.s = {
-	name = 'search',
-	f = {'<Cmd>Telescope find_files<CR>', 'find files'},
-	g = {'<Cmd>Telescope live_grep<CR>', 'live grep'},
-	b = {'<Cmd>Telescope buffers<CR>', 'buffers'},
-	h = {'<Cmd>Telescope help_tags<CR>', 'help tags'},
-}
-
-wkm.q = {
-	name = 'quit',
-	q = {'<Cmd>qa<CR>', 'exit editor'},
-}
-
-wkm.t = {
-	name = 'tabs',
-	c = {'<Cmd>tabnew<CR>', 'open a new tab'},
-	q = {'<Cmd>tabclose<CR>', 'close the current tab'},
-	Q = {'<Cmd>tabclose!<CR>', 'close the current tab with force'},
-	o = {'<Cmd>tabonly<CR>', 'close all other tabs but this one'},
-	O = {'<Cmd>tabonly!<CR>', 'close all other tabs but this one with force'},
-	p = {'<Cmd>tabprevious<CR>', 'go to previous tab (<S-C-left>)'},
-	n = {'<Cmd>tabnext<CR>', 'go to next tab (<S-C-right>)'},
-	t = {'<Cmd>tabs<CR>', 'show open tabs'},
-	P = {'<Cmd>tabmove -<CR>', 'move tab to the left'},
-	N = {'<Cmd>tabmove +<CR>', 'move tab to the right'},
-}
-
-wkm.w = {
-	name = 'windows',
-	d = {'<Cmd>q<CR>', 'close current window, quit vim if this is the last window'},
-	s = {'<Cmd>rightbelow split<CR>', 'split the current window horizontally'},
-	v = {'<Cmd>rightbelow vsplit<CR>', 'split the current window vertically'},
-	t = {'<C-W>T', 'move this window to a new tab page'},
-	h = {'<Cmd>:wincmd h<CR>', 'jump to the window to the left'},
-	j = {'<Cmd>:wincmd j<CR>', 'jump to the window below'},
-	k = {'<Cmd>:wincmd k<CR>', 'jump to the window above'},
-	l = {'<Cmd>:wincmd l<CR>', 'jump to the window to the right'},
-	H = {'<C-PageDown>', 'jump to the tab to the left'},
-	L = {'<C-PageUp>', 'jump to the tab to the right'},
-}
-
-wk.register(wkm, {prefix = '<leader>'})
+--vim.g.mapleader = ' '
+--vim.g.maplocalleader = ','
+--
+--local status, wk = pcall(require, 'which-key')
+--if not (status) then
+--    vim.notify('Could not load which-key, skipping mappings.')
+--    return
+--end
+--
+--local wkm = {}
+--
+--wkm.b = {
+--	name = 'buffers',
+--	b = {'<Cmd>Telescope buffers<CR>', 'select a buffer from the list'},
+--	v = {'<Cmd>rightbelow vsplit<CR><Cmd>Telescope buffers<CR>', 'select a buffer from the list, and open in a new vertical split'},
+--	h = {'<Cmd>rightbelow split<CR><Cmd>Telescope buffers<CR>', 'select a buffer from the list, and open in a new horizontal split'},
+--	d = {'<Cmd>bd<CR>', 'close the current buffer'},
+--	n = {'<Cmd>bn<CR>', 'jump to the next buffer'},
+--	p = {'<Cmd>bp<CR>', 'jump to the previous buffer'},
+--}
+--
+--wkm.c = {
+--	name = 'code and console',
+--	c = {'<Cmd>botright split term://fish<CR>i', 'open a terminal console'},
+--	f = {'<Cmd>call CocAction("format")<CR>', 'format the buffer'},
+--	r = {'<Plug>(coc-rename)', 'rename the object under the cursor'},
+--	i = {'<Cmd>CocCommand pyright.organizeimports<CR>', 'organize module imports'},
+--}
+--
+--wkm.f = {
+--	name = 'files',
+--	f = {'<Cmd>Telescope find_files<CR>', 'find files'},
+--	v = {'<Cmd>rightbelow vsplit<CR><Cmd>Telescope find_files<CR>', 'find file and open in new vertical split'},
+--	V = {'<Cmd>vsplit<CR><Cmd>Telescope find_files<CR>', 'find file and open in new vertical split'},
+--	h = {'<Cmd>rightbelow split<CR><Cmd>Telescope find_files<CR>', 'find file and open in new horizontal split'},
+--	H = {'<Cmd>split<CR><Cmd>Telescope find_files<CR>', 'find file and open in new horizontal split'},
+--	s = {'<Cmd>w<CR>', 'save to disk'},
+--	c = {'<Cmd>rightbelow split $MYVIMRC<CR>', 'load vim initialization file'},
+--	r = {'<Cmd>source $MYVIMRC<CR>', 'compile vim initialization'},
+--	n = {
+--		name = 'new-files',
+--		s = {'<Cmd>:rightbelow new<CR>', 'split new buffer into window horizontally'},
+--		v = {'<Cmd>:rightbelow vnew<CR>', 'split new buffer into window vertically'},
+--	}
+--}
+--
+--wkm.g = {
+--	name = 'git',
+--	g = {'<Cmd>:G<CR>', 'Open fugitive general buffer'}
+--}
+--
+--wkm.j = {
+--	name = 'jump',
+--	j = {'<Plug>(easymotion-overwin-f2)', 'Jump the cursor to any point by typing two characters.'},
+--}
+--
+--wkm.s = {
+--	name = 'search',
+--	f = {'<Cmd>Telescope find_files<CR>', 'find files'},
+--	g = {'<Cmd>Telescope live_grep<CR>', 'live grep'},
+--	b = {'<Cmd>Telescope buffers<CR>', 'buffers'},
+--	h = {'<Cmd>Telescope help_tags<CR>', 'help tags'},
+--}
+--
+--wkm.q = {
+--	name = 'quit',
+--	q = {'<Cmd>qa<CR>', 'exit editor'},
+--}
+--
+--wkm.t = {
+--	name = 'tabs',
+--	c = {'<Cmd>tabnew<CR>', 'open a new tab'},
+--	q = {'<Cmd>tabclose<CR>', 'close the current tab'},
+--	Q = {'<Cmd>tabclose!<CR>', 'close the current tab with force'},
+--	o = {'<Cmd>tabonly<CR>', 'close all other tabs but this one'},
+--	O = {'<Cmd>tabonly!<CR>', 'close all other tabs but this one with force'},
+--	p = {'<Cmd>tabprevious<CR>', 'go to previous tab (<S-C-left>)'},
+--	n = {'<Cmd>tabnext<CR>', 'go to next tab (<S-C-right>)'},
+--	t = {'<Cmd>tabs<CR>', 'show open tabs'},
+--	P = {'<Cmd>tabmove -<CR>', 'move tab to the left'},
+--	N = {'<Cmd>tabmove +<CR>', 'move tab to the right'},
+--}
+--
+--wkm.w = {
+--	name = 'windows',
+--	d = {'<Cmd>q<CR>', 'close current window, quit vim if this is the last window'},
+--	s = {'<Cmd>rightbelow split<CR>', 'split the current window horizontally'},
+--	v = {'<Cmd>rightbelow vsplit<CR>', 'split the current window vertically'},
+--	t = {'<C-W>T', 'move this window to a new tab page'},
+--	h = {'<Cmd>:wincmd h<CR>', 'jump to the window to the left'},
+--	j = {'<Cmd>:wincmd j<CR>', 'jump to the window below'},
+--	k = {'<Cmd>:wincmd k<CR>', 'jump to the window above'},
+--	l = {'<Cmd>:wincmd l<CR>', 'jump to the window to the right'},
+--	H = {'<C-PageDown>', 'jump to the tab to the left'},
+--	L = {'<C-PageUp>', 'jump to the tab to the right'},
+--}
+--
+--wk.register(wkm, {prefix = '<leader>'})
